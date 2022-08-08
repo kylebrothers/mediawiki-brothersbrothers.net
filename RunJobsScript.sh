a96e86ab923b50c2f244f8f04788888951b47f36
kylebrothers <43184070+kylebrothers@users.noreply.github.com>
2022-08-07 21:16:35 -0400

    Update RunJobsScript.sh

--- RunJobsScript.sh
+++ RunJobsScript.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-php /var/www/html/maintenance/runJobs.php --wait
 FILE=/var/www/html/extensions/SemanticMediaWiki/.smw.json
 if [ -f "$FILE" ]; then
     echo "$FILE exists."
 else 
     echo "$FILE does not exist."
-    php /var/www/html/maintenance/udpate.php --wait
+    /usr/bin/php /var/www/html/maintenance/udpate.php --wait
 fi
+/usr/bin/php /var/www/html/maintenance/runJobs.php --wait
